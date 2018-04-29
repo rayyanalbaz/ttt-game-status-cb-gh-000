c6e34f9b9efa0d73ae4d0569d027578c54c1efa4
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-04-29 03:52:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,12 @@ def won?(board)
         if board[index1] == "X" && board[index2] == "X" && board[index3] == "X"
           return win_combo
         end
+        if board[index1] == "X" && board[index2] == "X" && board[index3] == "X"
+          return win_combo
+        end
+        if board[index1] == "O" && board[index2] == "O" && board[index3] == "O"
+          return win_combo
+        end
       end
 
 
